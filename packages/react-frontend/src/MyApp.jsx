27afcbd357edf23c63bd5ec69d56d10952a972ac
nluong9600 <nluong9600@gmail.com>
2024-04-04 17:46:18 -0700

    Currently on Step 5

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,33 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+
+function MyApp() {
+	const [characters, setCharacters] = useState([
+		{
+			name: "Charlie",
+			job: "Janitor"
+		},
+		{
+			name: "Mac",
+			job: "Bouncer"
+		},
+		{
+			name: "Dee",
+			job: "Aspring actress"
+		},
+		{
+			name: "Dennis",
+			job: "Bartender"
+		}
+	]);
+
+	return (
+		<div className="container">
+		<Table characterData = {characters} />
+		</div>
+	);
+}
+
+export default MyApp;
